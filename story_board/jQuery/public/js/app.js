45b91e6bcab81837615efb533dcd404b6c101e2d
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-08-20 23:04:51 -0400

    Add files via upload

---
+++ story_board/jQuery/public/js/app.js
@@ -0,0 +1,45 @@
+(function(){
+    // Initialize Firebase
+    
+        var config = {
+            apiKey: "AIzaSyDNnreA9L89hgbGbZm333oF5tVBrZYcjlo",
+            authDomain: "test-54f7f.firebaseapp.com",
+            databaseURL: "https://test-54f7f.firebaseio.com",
+            projectId: "test-54f7f",
+            storageBucket: "test-54f7f.appspot.com",
+            messagingSenderId: "564818280226"
+        };
+        firebase.initializeApp(config);
+
+        //Get elements
+        const preObject = document.getElementById('object');
+        const ulList = document.getElementById('list');
+
+        //Create references
+        const dbRefObject = firebase.database().ref().child('object');
+        const dbRefList = dbRefObject.child('hobbies');
+
+        //Sync object changes
+        //dbRefObject.on('value', snap => console.log(snap.val()));
+        dbRefObject.on('value', snap =>{
+            preObject.innerText = JSON.stringify(snap.val(), null, 3);
+        });
+
+        //Sync list changes
+        dbRefList.on('child_added', snap => {
+            const li = document.createElement('li');
+            li.innerText = snap.val();
+            li.id = snap.key;
+            ulList.appendChild(li);
+        });
+
+         dbRefList.on('child_changed',snap => {
+             const liChanged = document.getElementById(snap.key);
+             liChanged.innerText = snap.val();
+         });
+
+         dbRefList.on('child_removed',snap => {
+             const liRemoved = document.getElementById(snap.key);
+             liRemoved.remove();
+         });
+}());
